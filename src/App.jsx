c3976463869a28946382a9db976f1be81619ce67
Dahrius93 <dariolauretta93@gmail.com>
2026-04-18 21:28:40 +0200

    ui fix

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,16 @@ import ConveyorBelt from './components/ConveyorBelt'
 
 function App() {
   return (
-    <div className="bg-violet-50 min-h-screen">
-      <h1 className="text-2xl sm:text-3xl font-bold text-center p-4 sm:p-6">PackML States</h1>
-      <StatesGrid />
-      <ConveyorBelt />
-      <OperatorPanel />
-    </div>
+    <>
+      <div className="bg-violet-50 min-h-screen">
+        <h1 className="text-2xl sm:text-3xl font-bold text-center p-4 sm:p-6">
+          PackML States <span className="text-xs">DLR</span>
+        </h1>
+        <StatesGrid />
+        <ConveyorBelt />
+        <OperatorPanel />
+      </div>
+    </>
   )
 }
 
